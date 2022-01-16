50f6e3ec708e5c941d600b5765fb1c085ec90072
Xuan-An Cao <caoxantb@gmail.com>
2022-01-16 22:47:55 +0200

    add exercises 6.21/using hooks

--- redux-anecdotes/src/reducers/notificationReducer.js
+++ redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,23 +1,25 @@
 const initialState = "";
+let timeOut;
 
 const notificationReducer = (state = initialState, action) => {
   if (action.type === "INIT_NOTIFICATION") return action.data;
   return state;
 };
 
-export const initializeNotificationActionCreators = (noti, timeOut) => {
+export const initializeNotificationActionCreators = (noti, ms = 0) => {
   return async (dispatch) => {
+    clearTimeout(timeOut);
     dispatch({
       type: "INIT_NOTIFICATION",
       data: noti,
     });
-    setTimeout(
+    timeOut = setTimeout(
       () =>
         dispatch({
           type: "INIT_NOTIFICATION",
           data: "",
         }),
-      timeOut
+      ms
     );
   };
 };
